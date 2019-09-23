3c686a48ec3c5f82ad6fc74e41c1185b00072bbe
Arbahud Rio Daroyni <roydotendless@gmail.com>
2019-09-23 19:53:17 +0700

    ok

---
+++ assets/MaterialKitAsset.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+class MaterialKitAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/material-kit/assets';
+    public $js = [
+        'js/core/jquery.min.js',
+        'js/core/popper.min.js',
+        'js/core/bootstrap-material-design.min.js',
+        'js/plugins/moment.min.js',
+        'js/plugins/bootstrap-datetimepicker.js',
+        'js/plugins/nouislider.min.js',
+        'js/material-kit.js?v=2.0.6'
+        // more plugin Js here
+    ];
+    public $css = [
+        'css/material-kit.css?v=2.0.6',
+        'demo/demo.css',
+        // more plugin CSS here
+    ];
+    public $depends = [
+    ];
+}
---
+++ assets/OnlineAsset.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+class OnlineAsset extends AssetBundle
+{
+    public $css = [
+        'https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css',
+        'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons',
+    ];
+}
